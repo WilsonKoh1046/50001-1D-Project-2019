6dc6d97b39de812ad7e6dfcc090c01d6449355ac
Wilson Koh <NikoKoh98@gmail.com>
2019-10-20 23:26:11 +0800

    initial commit

---
+++ app/src/main/java/com/example/a1dproject_campustradingapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a1dproject_campustradingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Test for firebase connection.
+        // To be deleted later.
+        Toast.makeText(MainActivity.this, "Firebase connection success!", Toast.LENGTH_LONG).show();
+    }
+}
