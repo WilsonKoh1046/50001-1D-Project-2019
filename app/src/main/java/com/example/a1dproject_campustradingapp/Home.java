44516cd06f57334e6039bdc19044473b6e389623
Wilson Koh <NikoKoh98@gmail.com>
2019-11-14 01:59:39 +0800

    News feed - main page

--- app/src/main/java/com/example/a1dproject_campustradingapp/Home.java
+++ app/src/main/java/com/example/a1dproject_campustradingapp/Home.java
@@ -1,8 +1,14 @@
 package com.example.a1dproject_campustradingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomBar = findViewById(R.id.bottom_navigation);
+        bottomBar.setOnNavigationItemSelectedListener(barListener);
+
+        // make news feed page as default page
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_section, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener barListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    // navigate through the bottom navigation
+                    /*
+                    // int id = menuItem.getItemId();
+                    if (id == R.id.nav_home) {
+                        selectedFragment = new HomeFragment();
+                    } */
+
+                    selectedFragment = new HomeFragment();
+                    // replace the empty fragment with the main news feed page
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_section, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
