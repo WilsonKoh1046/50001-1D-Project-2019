2e55fd7b631dfe52dc02de43010d7129b5149e50
Wilson Koh <NikoKoh98@gmail.com>
2019-11-13 15:38:43 +0800

    News Feed Page - bottom bar

---
+++ app/src/main/java/com/example/a1dproject_campustradingapp/Home.java
@@ -0,0 +1,14 @@
+package com.example.a1dproject_campustradingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
