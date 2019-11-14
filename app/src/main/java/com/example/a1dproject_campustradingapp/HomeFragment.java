1fc4875e5ea5a75f8f31d1e8466c2c921ea7c384
SybilD <ke_dong@mymail.sutd.edu.sg>
2019-11-15 03:46:51 +0800

    individual product_version 1

--- app/src/main/java/com/example/a1dproject_campustradingapp/HomeFragment.java
+++ app/src/main/java/com/example/a1dproject_campustradingapp/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.a1dproject_campustradingapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,10 +16,25 @@ import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+    Activity context;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        context=getActivity();
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        ImageView cd = (ImageView) context.findViewById(R.id.thumbnail);
+        cd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Product.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/a1dproject_campustradingapp/Product.java
@@ -0,0 +1,16 @@
+package com.example.a1dproject_campustradingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+
+
+}
