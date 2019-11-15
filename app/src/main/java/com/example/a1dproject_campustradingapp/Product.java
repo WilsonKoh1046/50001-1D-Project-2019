8b136a2dee78189d7cbe5047f8d644c4a1ff5b50
SybilD <ke_dong@mymail.sutd.edu.sg>
2019-11-15 15:16:51 +0800

    individual product_version 1 with horizontal picture scroll

--- app/src/main/java/com/example/a1dproject_campustradingapp/Product.java
+++ app/src/main/java/com/example/a1dproject_campustradingapp/Product.java
@@ -3,14 +3,52 @@ package com.example.a1dproject_campustradingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Product extends AppCompatActivity {
+    private LinearLayout mGallery;
+    private int[] mImgIds;
+    private LayoutInflater mInflater;
+    private HorizontalScrollView horizontalScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        mInflater = LayoutInflater.from(this);
+        initData();
+        initView();
+    }
+
+    private void initData()
+    {
+        mImgIds = new int[] { R.drawable.product_book1, R.drawable.product_book2, R.drawable.product_book3
+        };
+    }
+
+    private void initView()
+    {
+        mGallery = (LinearLayout) findViewById(R.id.linear);
+
+        for (int i = 0; i < mImgIds.length; i++)
+        {
+
+            View view = mInflater.inflate(R.layout.product_item,
+                    mGallery, false);
+            ImageView img = (ImageView) view
+                    .findViewById(R.id.id_index_gallery_item_image);
+            img.setImageResource(mImgIds[i]);
+            mGallery.addView(view);
+        }
     }
 
 
-}
+
+    }
+
+
+
