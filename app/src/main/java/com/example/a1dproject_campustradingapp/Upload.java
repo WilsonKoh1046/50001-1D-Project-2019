5a770481ba5c4c1222f9f5f80d760b2255eab21d
jessiexiye <li.jiaxi97@gmail.com>
2019-11-22 20:00:03 +0800

    Upload image and retrieve image page

---
+++ app/src/main/java/com/example/a1dproject_campustradingapp/Upload.java
@@ -0,0 +1,49 @@
+package com.example.a1dproject_campustradingapp;
+
+import com.google.firebase.database.Exclude;
+
+public class Upload {
+    private String mName;
+    private String mImageUrl;
+    private String mKey;
+
+    public Upload(){
+        //empty constructor needed to work with firebase database
+    }
+
+    public Upload(String name, String imageUrl){
+        if(name.trim().equals("")){
+            name = "No Name";
+        }
+        mName = name;
+        mImageUrl = imageUrl;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String name){
+        mName = name;
+    }
+
+    public String getmImageUrl(){
+        return mImageUrl;
+    }
+
+    public void setmImageUrl(String imageUrl){
+        mImageUrl = imageUrl;
+    }
+
+    @Exclude
+    public String getmKey(){
+        return mKey;
+    }
+
+    @Exclude
+    public void setmKey(String key){
+        mKey = key;
+    }
+}
+
+
